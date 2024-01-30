8214c8b8c86e80de749b063be1741dcd2ec62a08
mohitps <psmohit7@gmail.com>
2024-01-30 18:54:51 -0500

    modified the home page; fixed redirects in App.js

--- weather-ui/src/App.js
+++ weather-ui/src/App.js
@@ -1,61 +1,26 @@
-// React imports
-import './App.css';
-import React, { useState, useEffect, useMemo, useCallback, memo } from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-// import { Container, Form, Button, ListGroup } from 'react-bootstrap';
+import { Container } from "react-bootstrap";
+import { Routes, Route } from "react-router-dom";
 
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+// Layout
+import Layout from "./layout/Layout";
 
-// Image imports
-import logo from './images/logo-weather-200.png'
-import stormCloud from './images/storm-cloud-picture.jpg'
+// pages
+import Home from "./pages/Home";
+import Weather from "./pages/Weather";
+import NotFound from "./pages/NotFound";
 
-// Component imports
-import MagicLink from './components/MagicLink';
-import Profile from './components/Profile';
-import WorkoutList from './components/Workouts';
-import WeatherPage from './components/WeatherPage';
-
-
-
-function App() {
-
+const App = () => {
   return (
-    // split the main page into left and right halves
-    // <div className="split-page" style={{ backgroundColor: '#343d46' }}>
-    //   <div className="left-container" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-    //     <img src={stormCloud} alt="storm-cloud" style={{ display: 'block', height: '90%', width: '100%', borderRadius: '15px'}} />
-    //   </div>
-
-    //   <div className="right-container" style={{ textAlign: 'center' }}>
-
-    //     {/* Add Logo to right container */}
-    //     <img src={logo} alt="website-logo" style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto', marginTop: '20%', width: '30%' }} />
-    //     <h1 className='weather-app-name'>ClimaFit</h1>
-
-    //     {/* Login Button */}
-    //     <button type="button" className="btn btn-primary btn-lg">Login</button>
-
-    //     {/* Search Bar */}
-    //     {/* <div class="md-form mt-0" style={{ width: "50%", margin: 'auto'}}>
-    //       <input class="form-control" type="text" placeholder="Search ZIP Code" aria-label="Search" />
-    //     </div> */}
-
-    //   </div>
-    // </div>
-
-    // <MagicLink></MagicLink>
-    // <Profile></Profile>
-    // <WorkoutList></WorkoutList>
-    <WeatherPage></WeatherPage>
-    // <FetchAPI></FetchAPI>
-
+    <Layout>
+      <Container>
+        <Routes>
+          <Route path="/" element={<Home />} exact />
+          <Route path="/weather" element={<Weather />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Container>
+    </Layout>
   );
-}
+};
 
 export default App;
---
+++ weather-ui/src/pages/Home.js
@@ -0,0 +1,16 @@
+import Header from '../components/Header'
+import Meta from '../components/Meta'
+
+const Home = () => {
+  const pageTitle = 'Home'
+  const pageDescription = 'Welcome to weather application'
+
+  return (
+    <div>
+      <Meta title={pageTitle}/>
+      <Header head={pageTitle} description={pageDescription} />
+    </div>
+  )
+}
+
+export default Home
